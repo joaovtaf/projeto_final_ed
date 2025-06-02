714821b5368478fe4cc9b6b7a711d988e28c54c6
dcastanheiro <dilmar.castanheiro@gmail.com>
2025-06-02 13:37:09 -0300

    função destroy pronta

--- src/bst.cpp
+++ src/bst.cpp
@@ -12,13 +12,28 @@ BinaryTree* create(){
 }
 
 InsertResult insert(BinaryTree* tree, const std::string& word, int documentId){
-
+    
 }
 
 SearchResult search(BinaryTree* tree, const std::string& word){
 
 }
 
-void destroy(BinaryTree* tree){
+void destroyNode(Node* node) {
+    if (node != nullptr) {
+        destroyNode(node->left);
+        destroyNode(node->right);
+        delete node;
+    }
+    return; // recursivamente deleta todos os nós
+}
+
+void destroy(BinaryTree* tree) {
+
+    if (tree == nullptr) return; // se a arvore for null retorna direto
+
+    destroyNode(tree->root); // recursivamente deleta os nós
     
+    delete tree; 
+    return; // deleta a tree e root, e retorna
 }
