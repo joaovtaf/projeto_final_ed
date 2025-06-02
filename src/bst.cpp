db11d7d6212dd6260b33db2f90a22cb2e55f4fc4
dcastanheiro <dilmar.castanheiro@gmail.com>
2025-06-02 15:16:16 -0300

    comentarios na

--- src/bst.cpp
+++ src/bst.cpp
@@ -13,18 +13,20 @@ BinaryTree* create() {
 }
 
 InsertResult insert(BinaryTree* tree, const std::string& word, int documentId){
-    auto start = std::chrono::high_resolution_clock::now();
+    auto start = std::chrono::high_resolution_clock::now(); // incia a contagem
     int numComparisons = 0;
     
     if(tree->root == nullptr) {
         tree->root = new Node{word, {documentId}, nullptr, nullptr, nullptr, 0, 0};
+
         auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> elapsed = end - start;
+        std::chrono::duration<double> elapsed = end - start; // salva o tempo de execução
+
         return InsertResult{numComparisons, elapsed.count()};
     } // se a árvore ainda estiver vazia é criado o root
 
-    Node* current_node = tree->root;
-    Node* current_node_parent = nullptr;
+    Node* current_node = tree->root; // nó atual
+    Node* current_node_parent = nullptr; // guarda o pai do nó atual
     int L_or_R = 0; // -1 de left e +1 se right 
 
     while (current_node != nullptr) {
@@ -32,28 +34,33 @@ InsertResult insert(BinaryTree* tree, const std::string& word, int documentId){
         numComparisons++;
 
         if ( comparison == 0 ) {
-            current_node->documentIds.push_back(documentId);
+            current_node->documentIds.push_back(documentId); // salva o Id
+
             auto end = std::chrono::high_resolution_clock::now();
-            std::chrono::duration<double> elapsed = end - start;
-            return InsertResult{numComparisons, elapsed.count()};
+            std::chrono::duration<double> elapsed = end - start; // salva o tempo de execução
+
+            return InsertResult{numComparisons, elapsed.count()}; // se a palavra for igual, retorna com a lista atualizada
         }
         if ( comparison < 0 ) {
             current_node_parent = current_node;
             current_node = current_node->left;
-            L_or_R = -1;
+            L_or_R = -1; // se a palavra for menor, segue pela esquerda
         }
         if ( comparison > 0 ) {
             current_node_parent = current_node;
             current_node = current_node->right;
-            L_or_R = +1;
+            L_or_R = +1; // se a palavra for maior, segue pela direita
         }
     }
     
     current_node = new Node{word, {documentId}, current_node_parent, nullptr, nullptr, 0, 0};
+
     if (L_or_R == -1) current_node_parent->left = current_node;
-    if (L_or_R == +1) current_node_parent->right = current_node; 
+    if (L_or_R == +1) current_node_parent->right = current_node; // guarda o nó atual do lado corrto do nó pai
+
     auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed = end - start;
+    std::chrono::duration<double> elapsed = end - start; // salva o tempo de execução
+
     return InsertResult{numComparisons, elapsed.count()};
 }
 
