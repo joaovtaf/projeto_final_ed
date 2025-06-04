35e1269cbb2833942ec6fa6dd347a8a26d3f6745
dcastanheiro <dilmar.castanheiro@gmail.com>
2025-06-04 10:49:31 -0300

    Comentários na bst.h

--- src/bst.h
+++ src/bst.h
@@ -9,12 +9,19 @@
 
 namespace BST{
 
+// Cria uma nova árvore binária vazia.
+// Retorna uma BinaryTree.
 BinaryTree* create();
 
+// Insere uma palavra na árvore associada a um documentId. Se for nova cria um novo nó, e já existir adiciona o documentId.
+// Retorna um InsertResult que trás estatísticas sobre o desempenho (número de comparações e tempo de execução).
 InsertResult insert(BinaryTree* tree, const std::string& word, int documentId);
 
+// Realiza uma busca em uma árvore.
+// Retorna um SearchResult que trás se a palavra foi achada, em quais documentos está, e estatísticas sobre o desempenho (número de comparações e tempo de execução).
 SearchResult search(BinaryTree* tree, const std::string& word);
 
+// Libera toda a memória alocada pela árvore binária.
 void destroy(BinaryTree* tree);
 }
 
